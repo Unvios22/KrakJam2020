4fc417d6779fe7c34c24a0fac197711e190a45bd
Kajetan Jóźwiak <unvios22@gmail.com>
2020-02-20 16:04:16 +0100

    Add volume slider to pause menu

---
+++ KrakJam2020/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+    [SerializeField] private AudioMixer mainAudioMixer;
+    [SerializeField] private string exposedVolumeParamName;
+    [SerializeField] private Slider _slider;
+    
+    private void Start() {
+        mainAudioMixer.GetFloat(exposedVolumeParamName, out float volume);
+        _slider.value = volume;
+    }
+
+    public void SetAudioVolume() {
+        mainAudioMixer.SetFloat(exposedVolumeParamName, _slider.value);
+    }
+}
